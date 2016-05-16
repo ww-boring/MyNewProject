cd4b56331b2503111228131e6adec6f47497bf47
Antit <Antit@DESKTOP-5ARPDL2>
2016-05-16 22:26:39 +0200

    Add project files.

---
+++ MyNewProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyNewProject.MvcApplication" Language="C#" %>
---
+++ MyNewProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyNewProject.Startup))]
+namespace MyNewProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
